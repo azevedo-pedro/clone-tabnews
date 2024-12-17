8da47abb4511d746e5716824a7e59419f42764b2
Pedro Azevedo <135284315+azevedo-pedro@users.noreply.github.com>
2024-12-17 16:19:19 +0000

    try fix ci connection error

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -2,30 +2,28 @@ import database from "infra/database";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
-  await database.query("drop schema public cascade; create schema public;");
   await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
 });
 
 test("POST to /api/v1/migrations should return 200", async () => {
-  const responseOne = await fetch(
-    `http://localhost:3000/api/v1/migrations`,
-    {
-      method: "POST",
-    },
-  );
-  expect(responseOne.status).toBe(201);
-  const responseOneBody = await responseOne.json();
-  expect(Array.isArray(responseOneBody)).toBe(true);
-  expect(responseOneBody.length).toBeGreaterThan(0);
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
 
-  const responseTwo = await fetch(
-    `http://localhost:3000/api/v1/migrations`,
-    {
-      method: "POST",
-    },
-  );
-  expect(responseTwo.status).toBe(200);
-  const responseTwoBody = await responseTwo.json();
-  expect(Array.isArray(responseTwoBody)).toBe(true);
-  expect(responseTwoBody.length).toBe(0);
+  const response1Body = await response1.json();
+
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
 });
