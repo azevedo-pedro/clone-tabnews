9e76188b1a929d8ff700dcbc18693161a66a16bd
Pedro Azevedo <135284315+azevedo-pedro@users.noreply.github.com>
2024-12-17 16:07:57 +0000

    reduce max timout to 1000

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,18 +6,18 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
-      maxTimeout: 5000,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
-      const response = await fetch(`http://localhost:3000/api/v1/status`);
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
       if (response.status !== 200) {
         throw Error();
       }
     }
   }
 }
-
 export default {
   waitForAllServices,
 };
