47b1584435f73e3c266ddb1f05b61e6d278dd5ad
Pedro Abreu de Azevedo <azevedodev@outlook.com>
2025-01-04 19:04:13 -0300

    feat: add new away to saw database status

--- pages/status/index.js
+++ pages/status/index.js
@@ -6,13 +6,30 @@ async function get(key) {
 }
 
 export default function StatusPage() {
-  const { data } = useSWR("/api/v1/status", get, {
+  const { data, isLoading } = useSWR("/api/v1/status", get, {
     refreshInterval: 2000,
   });
+  const formattedDate = new Date(data.update_at).toLocaleString();
+
   return (
     <div>
       <h1>Status</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {isLoading ? (
+        <p>Carregando...</p>
+      ) : (
+        <>
+          <p>Última atualização: {formattedDate}</p>
+          <p>Versão: {data.dependencies.database.version}</p>
+          <p>
+            Número máximo de conexões:
+            {data.dependencies.database.max_connections}
+          </p>
+          <p>
+            Número de conexões abertas:
+            {data.dependencies.database.opened_connections}
+          </p>
+        </>
+      )}
     </div>
   );
 }
